7f2698f8c5c6f1b7dd1c8414e803763cd464be87
fazriarisap2 <fazriaprilianto.dev@gmail.com>
2021-05-04 17:23:41 +0700

    menambahkan angular

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose  = require('mongoose');
+const config = require('./config/database');
+
+mongoose.Promise = global.Promise
+mongoose.connect(config.uri,(err) => {
+    if (err) {
+        console.log('could not connect to database',err)
+    } else {
+        console.log('connected to database'+config.db)
+    }
+});
+
+app.set('port', process.env.port || 3000) 
+
+app.get('/', (req, res, next) =>{
+    res.send('<h1>Hello world<h1>');
+})
+
+app.listen(app.get('port'), server =>{
+    console.info(`Server listen on port ${app.get('port')}`);
+})
